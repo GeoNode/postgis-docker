858f99f674b7fffe04b1c381b5ef406e7afc220d
afabiani <alessio.fabiani@geosolutionsgroup.com>
2023-06-21 14:49:54 +0200

    - Updating the init_db script in order to be compatible with the new version of PostGIS

--- initdb-geonode.sh
+++ initdb-geonode.sh
@@ -3,24 +3,43 @@ set -e
 
 function create_geonode_user_and_database() {
 	local db=$1
-	echo "  Creating user and database '$db'"
+	local geonode_user="${GEONODE_DATABASE_USER:-$db}"
+	local geonode_password="$GEONODE_DATABASE_PASSWORD"
+	echo "  Creating user and database '$db' with Geonode user: $geonode_user"
 	psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
 		CREATE USER $db;
-		ALTER USER $db with encrypted password '$GEONODE_DATABASE_PASSWORD';
+		ALTER USER $geonode_user with encrypted password '$geonode_password';
+		ALTER USER $geonode_user CREATEDB;
 		CREATE DATABASE $db;
-		GRANT ALL PRIVILEGES ON DATABASE $db TO $db;
+		GRANT CREATE ON SCHEMA public TO $geonode_user;
+		GRANT USAGE ON SCHEMA public TO $geonode_user;
+		GRANT ALL PRIVILEGES ON DATABASE $db TO $geonode_user;
 EOSQL
+	psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" -d "$db" -c "GRANT ALL ON SCHEMA public TO $geonode_user;"
+	psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" -d "$db" -c "GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO $geonode_user;"
+	psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" -d "$db" -c "GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO $geonode_user;"
+	psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" -d "$db" -c "GRANT INSERT, SELECT, UPDATE, DELETE ON ALL TABLES IN SCHEMA public TO $geonode_user;"
 }
 
 function create_geonode_user_and_geodatabase() {
 	local geodb=$1
-	echo "  Creating user and database '$geodb'"
+	local geonode_geodb_user="${GEONODE_GEODATABASE_USER:-$geodb}"
+	local geonode_geodb_password="$GEONODE_GEODATABASE_PASSWORD"
+
+	echo "  Creating user and database '$geodb' with Geonode GeoDB user: $geonode_geodb_user"
 	psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
 		CREATE USER $geodb;
-		ALTER USER $geodb with encrypted password '$GEONODE_GEODATABASE_PASSWORD';
+		ALTER USER $geonode_geodb_user with encrypted password '$geonode_geodb_password';
+		ALTER USER $geonode_geodb_user CREATEDB;
 		CREATE DATABASE $geodb;
-		GRANT ALL PRIVILEGES ON DATABASE $geodb TO $geodb;
+		GRANT CREATE ON SCHEMA public TO $geonode_geodb_user;
+		GRANT USAGE ON SCHEMA public TO $geonode_geodb_user;
+		GRANT ALL PRIVILEGES ON DATABASE $geodb TO $geonode_geodb_user;
 EOSQL
+	psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" -d "$geodb" -c "GRANT ALL ON SCHEMA public TO $geonode_geodb_user;"
+	psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" -d "$geodb" -c "GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO $geonode_geodb_user;"
+	psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" -d "$geodb" -c "GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO $geonode_geodb_user;"
+	psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" -d "$geodb" -c "GRANT INSERT, SELECT, UPDATE, DELETE ON ALL TABLES IN SCHEMA public TO $geonode_geodb_user;"
 }
 
 function update_database_with_postgis() {
